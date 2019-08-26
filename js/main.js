45f2931aabbf507188e3106bf4b0e89cc91776d3
mdcowan <mdcowan@student.fullsail.edu>
2019-08-25 23:03:45 -0400

    HTML, CSS and JavaScript update

--- js/main.js
+++ js/main.js
@@ -10,22 +10,23 @@ window.addEventListener("load", function () {
 class AssignPrototype{
     constructor() {
         const page = document.querySelector("body");
+        const contactForm = page.querySelector('#contact form');
+        const contactSubmit = contactForm.querySelector('button');
+        console.log(contactSubmit);
 
         // Contact Form Submit Button Validation
         function validateForm(){
-            let contactForm = page.querySelector('#contact form');
             let name = contactForm.querySelector('input[name=name]');
             let email = contactForm.querySelector('input[name=email]');
+            let phone = contactForm.querySelector('input[name=phone]');
             let message = contactForm.querySelector('textarea[name=message]');
-            let submit = contactForm.querySelector('button');
             let valid = false;
             console.log("Validity State: " + name.validity.valid);
             //allow submit
-            let alertText = contactForm.querySelector('p')
+            let alertText = contactForm.querySelector('p');
             if (name.validity.valid === true){
                 if (email.validity.valid === true){
                     if(message.validity.valid === true){
-                        submit.classList.remove("disabled");
                         valid  = true;
                     }
                 }
@@ -35,10 +36,19 @@ class AssignPrototype{
                 let newAlert = document.createElement("p");
                 let text = document.createTextNode("Missing required field. Please update");
                 newAlert.appendChild(text);
+                newAlert.setAttribute("class", "alert");
                 contactForm.insertAdjacentElement("beforeend", newAlert);
             }
             else{
-                alertText.parentNode.removeChild(alertText);
+                contactSubmit.classList.remove("disabled");
+                name.value = "";
+                email.value = "";
+                phone.value = "";
+                message.value = "";
+
+                if(alertText){
+                    alertText.parentNode.removeChild(alertText);
+                }
             }
 
             return valid;
@@ -46,8 +56,83 @@ class AssignPrototype{
 
         //Submit Contact Form event handler
         function addContact(event){
-            let
+            event.preventDefault();
+            let valid = validateForm();
+            let message = "<div><h2>Thank you!</h2><p>We'll get back to you soon!</p></div>";
+
+            if(valid){
+                contactSubmit.setAttribute("class", "disabled");
+                contactSubmit.insertAdjacentHTML("afterend", message);
+            }
         }
+
+        document.addEventListener('invalid', (function(){
+            return function(e){
+                //prevent the browser from showing default error bubble/ hint
+                e.preventDefault();
+            };
+        })(), true);
+
+        contactSubmit.addEventListener('click', addContact);
+
+        /* --------------- Mobile Accordion -------------------------*/
+        function viewContent(event){
+            event.preventDefault();
+            console.log(event);
+            let section;
+
+            switch(event.target.id){
+                case 'head_whatis':
+                    section = page.querySelector('#whatis');
+                    break;
+                case 'head_why':
+                    section = page.querySelector('#why');
+                    break;
+                case 'head_services':
+                    section = page.querySelector('#services');
+                    break;
+                case 'head_blog':
+                    section = page.querySelector('#blog');
+                    break;
+                case 'head_contact':
+                    section = page.querySelector('#contact');
+                    break;
+            }
+
+            console.log(event.target.id);
+
+            if(section){
+                let content = section.querySelectorAll('.content_display');
+                let short_description = section.querySelector('.mobile_discription');
+
+                switch(content[0].style.display){
+                    case 'block':
+                        short_description.style.display = "block";
+                        break;
+                    default:
+                        short_description.style.display = "none";
+                        break;
+                }
+
+                for(let c of content){
+                    switch(c.style.display){
+                        case 'block':
+                            c.style.display = "none";
+                            break;
+                        default:
+                            c.style.display = "block";
+                    }
+                }
+
+
+            }
+        }
+
+        const sections = page.querySelectorAll('.mobile_nav');
+        for(let e of sections){
+            e.addEventListener('click', viewContent);
+        }
+
     }
 
     static getInstance(){
@@ -58,7 +143,7 @@ class AssignPrototype{
             return AssignPrototype._instance;
         }
         else{
-            throw "Error: Singleton already exists."
+            throw "Error: Singleton already exists.";
         }
     }
 
